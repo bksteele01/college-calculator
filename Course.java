ad212918232a021ba6301d0abb1c54a89d8b2ff5
Ben <bksteele01@gmail.com>
2022-02-14 19:51:57 +0000

    added structure for the two classes

--- Course.java
+++ Course.java
@@ -1,6 +1,19 @@
 public class Course{
-	
-	public static void Course(){
-
+	String name;
+	int credits;
+	String grade;
+	public Course(String name, int credits, String grade){
+		this.name = name;
+		this.credits = credits;
+		this.grade = grade;
+		}
+	public String returnName(){
+		return name;
+	}
+	public int returnCredits(){
+		return credits;
+	}
+	public String returnGrade(){
+		return grade;
 	}
 }
--- Semester.java
+++ Semester.java
@@ -1,4 +1,21 @@
+import java.util.*;
 public class Semester{
-	public static void Semester(){
-}
+	Course course1;
+	Course course2;
+	Course course3;
+	Course course4;
+	Course course5;
+	List<Course> semesterClasses = new ArrayList<>();
+	public Semester(Course course1, Course course2, Course course3, Course course4, Course course5){
+		this.course1 = course1;
+		this.course2 = course2;
+		this.course3 = course3;
+		this.course4 = course4;
+		this.course5 = course5;
+		semesterClasses.add(course1);
+		semesterClasses.add(course2);
+		semesterClasses.add(course3);
+		semesterClasses.add(course4);
+		semesterClasses.add(course5);
+	}
 }
